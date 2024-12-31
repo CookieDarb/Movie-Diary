58539848c45291a3abfc5dae6e0f9e03df5f843d
CookieDarb <darkblueauror@gmail.com>
2024-12-31 20:38:11 +0530

    path change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '/assets/react.svg'
 import viteLogo from '/vite.svg'
 import '/src/style/App.css'
 import Header from './components/Header'
